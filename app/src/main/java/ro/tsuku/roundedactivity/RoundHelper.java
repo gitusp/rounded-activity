e8dfa8d1f4aab4e63af5f848ab9033fe8bf52fb9
usp <usui@lemo.is>
2015-04-23 22:22:18 +0900

    import

---
+++ app/src/main/java/ro/tsuku/roundedactivity/RoundHelper.java
@@ -0,0 +1,53 @@
+package ro.tsuku.roundedactivity;
+
+import android.app.Activity;
+import android.graphics.Rect;
+import android.os.Build;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+/**
+ * Created by usp on 4/23/15.
+ */
+public class RoundHelper {
+    /**
+     * Clips corners of the given activity
+     * @param activity
+     */
+    public static void round(final Activity activity) {
+        final Window window = activity.getWindow();
+        final View decorView = window.getDecorView();
+        final ViewTreeObserver viewTreeObserver = decorView.getViewTreeObserver();
+
+        // observing the end of its layout
+        viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                // getting the screen information
+                Rect usableRect = new Rect();
+                decorView.getWindowVisibleDisplayFrame(usableRect);
+                int bottom = activity.findViewById(Window.ID_ANDROID_CONTENT).getBottom();
+                // creating imageview
+                ImageView imageView = new ImageView(activity);
+                imageView.setImageResource(R.drawable.rounded_frame);
+                imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                // creating layout params
+                FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(usableRect.width(), bottom - usableRect.top);
+                layoutParams.setMargins(0, usableRect.top, 0, 0);
+                imageView.setLayoutParams(layoutParams);
+                ((ViewGroup) decorView).addView(imageView);
+                // removing this listener
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                    viewTreeObserver.removeGlobalOnLayoutListener(this);
+                } else {
+                    viewTreeObserver.removeOnGlobalLayoutListener(this);
+                }
+            }
+        });
+
+    }
+}
