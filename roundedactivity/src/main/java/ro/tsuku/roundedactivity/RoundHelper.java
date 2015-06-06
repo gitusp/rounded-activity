bb9cef23104fdfd5199edbf1fedbc95ff1606324
usp <ususso@gmail.com>
2015-06-07 00:03:26 +0900

    bugfix

--- roundedactivity/src/main/java/ro/tsuku/roundedactivity/RoundHelper.java
+++ roundedactivity/src/main/java/ro/tsuku/roundedactivity/RoundHelper.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
@@ -20,31 +21,35 @@ public class RoundHelper {
      */
     public static void round(final Activity activity) {
         final Window window = activity.getWindow();
-        final View decorView = window.getDecorView();
+        final ViewGroup decorView = (ViewGroup) window.getDecorView();
 
-        // observing the end of its layout
+        // Create image view.
+        final ImageView imageView = new ImageView(activity);
+        imageView.setImageResource(R.drawable.rounded_frame);
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        decorView.addView(imageView);
+
+        // observing the end of its layout.
         decorView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
                 // getting the screen information
                 Rect usableRect = new Rect();
                 decorView.getWindowVisibleDisplayFrame(usableRect);
-                int bottom = activity.findViewById(Window.ID_ANDROID_CONTENT).getBottom();
-                // creating imageview
-                ImageView imageView = new ImageView(activity);
-                imageView.setImageResource(R.drawable.rounded_frame);
-                imageView.setScaleType(ImageView.ScaleType.FIT_XY);
                 // creating layout params
-                FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(usableRect.width(), bottom);
-                layoutParams.setMargins(0, usableRect.top, 0, 0);
-                imageView.setLayoutParams(layoutParams);
-                ((ViewGroup) decorView).addView(imageView);
-                // removing this listener
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-                    decorView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                FrameLayout.LayoutParams layoutParams;
+                if ((window.getAttributes().flags & WindowManager.LayoutParams.FLAG_FULLSCREEN) != 0) {
+                    // Full screen.
+                    // HACK: getWindowVisibleDisplayFrame doesn't reflect status bar immediately.
+                    layoutParams = new FrameLayout.LayoutParams(usableRect.width(), usableRect.height() + usableRect.top);
+                    layoutParams.setMargins(usableRect.left, 0, 0, 0);
                 } else {
-                    decorView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    // Not full screen.
+                    layoutParams = new FrameLayout.LayoutParams(usableRect.width(), usableRect.height());
+                    layoutParams.setMargins(usableRect.left, usableRect.top, 0, 0);
                 }
+                // Apply layout params.
+                imageView.setLayoutParams(layoutParams);
             }
         });
 
